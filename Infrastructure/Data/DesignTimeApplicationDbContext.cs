e184694fd1826f79e734ca7abdcf06a9ec478831
khuongdp <khuongdp1402@gmail.com>
2025-07-10 16:53:35 +0700

    remove connection string

--- Infrastructure/Data/DesignTimeApplicationDbContext.cs
+++ Infrastructure/Data/DesignTimeApplicationDbContext.cs
@@ -8,10 +8,6 @@ public class DesignTimeApplicationDbContext : IDesignTimeDbContextFactory<Applic
     public ApplicationDbContext CreateDbContext(string[] args)
     {
         var builder =new DbContextOptionsBuilder<ApplicationDbContext>();
-        //builder.UseSqlServer(@"Server=49.212.161.31;Initial Catalog=FBT_DEV2;Persist Security Info=False;User ID=sa;Password=@shuei249;MultipleActiveResultSets=True;Encrypt=True;TrustServerCertificate=True;Connection Timeout=300;", b =>
-        //{
-        //    b.MigrationsHistoryTable("__EFMigrationsHistoryWMS");
-        //})
         builder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=AHD;Trusted_Connection=True;", b =>
         {
             b.MigrationsHistoryTable("__EFMigrationsHistoryWMS");
